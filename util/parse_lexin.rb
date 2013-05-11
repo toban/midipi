441870eb24eabbf74661093d5f5f2727c6d2d657
toban <tobias.ande@gmail.com>
2013-05-11 17:06:53 +0000

    stash

--- util/parse_lexin.rb
+++ util/parse_lexin.rb
@@ -1,4 +1,4 @@
 #! /usr/bin/ruby
 require 'rexml/document'
-doc = REXML::Document.new File.new('../dictionary/LEXIN.xml')
+doc = REXML::Document.new File.new('../dictionary/LEXIN_test.xml')
 puts doc.elements('*/lemma-entry').count #{ |element| puts element.get_text }
