18c08bc34d8fad8777e7805ae47b8747c6a3fc47
tobias <tobias.ande@gmail.com>
2013-10-14 18:26:43 +0200

    use sqlitedb

---
+++ util/create_db.rb
@@ -0,0 +1,34 @@
+#! /usr/bin/ruby
+require File.join(File.dirname(__FILE__), 'parse_cmu.rb')
+require 'sqlite3'
+require 'htmlentities'
+
+class CreateDatabase
+
+	attr_accessor :cmu_parser, :db
+	
+	def initialize
+		@cmu_parser = CmuParser.new
+		
+		@db = SQLite3::Database.new("cmu.db")
+		@db.execute("CREATE TABLE IF NOT EXISTS words (word_stripped TEXT NOT NULL PRIMARY KEY, word_encoded TEXT, code TEXT NOT NULL);")
+		puts "init database"
+	end
+	
+	def seed
+	encoder = HTMLEntities.new
+	@cmu_parser.word_hash.keys.sort.each do |key|
+			#puts "#{key}-----"
+			val = @cmu_parser.word_hash[key]
+			
+			@db.execute("INSERT OR IGNORE INTO words (word_stripped, word_encoded, code) VALUES ('%s', '%s', '%s');"  % [val.name.gsub(/[^0-9a-z ]/i, ''), encoder.encode(val.name), val.code.to_s])
+			
+		end
+  
+  end
+	
+
+end
+
+c = CreateDatabase.new
+c.seed
