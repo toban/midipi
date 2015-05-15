9c9bf4bae530ae9f71c6d2e748131167ef1ec5f7
Tobias Andersson <tobias.ande@gmail.com>
2015-05-15 08:41:25 +0200

    add

---
+++ src/build_to_pi.rb
@@ -0,0 +1,28 @@
+#! /usr/bin/ruby
+require 'rubygems' 
+require 'net/ssh' 
+require 'net/scp'  
+HOST = '192.168.1.4' 
+USER = 'pi' 
+PASS = 'Kickflip.!"#' 
+
+remote_path = "/home/pi/midipi"
+exec_path = "/src/midipi_daemon.rb restart"
+puts "Connecting ..."
+Net::SSH.start( HOST, USER, :password => PASS ) do|ssh|
+	puts "Connected ..."
+	result = ssh.exec!('cd /home/pi/midipi/src/; sudo ./midipi_daemon.rb stop')
+	puts result 
+	result = ssh.exec!('rm -rf ' + remote_path) 
+	puts result 
+	puts "Delete old files ..."
+	puts "Upload started ..."
+	Net::SCP.upload!(HOST, USER, "../../midipi", remote_path, :ssh => { :password => PASS}, :recursive=> true, :verbose=>true)
+	puts "Upload finished ..."
+	#result = ssh.exec!('cd /home/pi/midipi/src/; sudo ./midipi_daemon.rb start')
+	#result = ssh.exec!('')
+	puts result 
+end
+
+
+#exec( "ls ../../midipi" )
